69ed205978059ffddc99c07c450a63de399383be
Bia10 <t.locke1234@gmail.com>
2022-02-17 15:35:16 +0100

    cleanup/update Varps

--- src/main/java/rsb/internal/globval/GlobalSettingValues.java
+++ src/main/java/rsb/internal/globval/GlobalSettingValues.java
@@ -1,34 +1,45 @@
 package rsb.internal.globval;
 
 public class GlobalSettingValues {
+    public static final int SETTING_TOGGLE_LOOP_MUSIC = 19;  
     public static final int SETTING_COMBAT_STYLE = 43;
+    public static final int SETTING_PRAYER_THICK_SKIN = 83;
+    public static final int SETTING_QUEST_POINTS = 101;
     public static final int SETTING_POISON = 102;
+    public static final int SETTING_BANK_TOGGLE_WITHDRAW_MODE = 115;
+    public static final int SETTING_TYPE_SHOP = 118; 
     public static final int SETTING_ADJUST_SCREEN_BRIGHTNESS = 166;
     public static final int SETTING_ADJUST_MUSIC_VOLUME = 168;
     public static final int SETTING_ADJUST_SOUND_EFFECT_VOLUME = 169;
     public static final int SETTING_MOUSE_BUTTONS = 170;
     public static final int SETTING_CHAT_EFFECTS = 171;
     public static final int SETTING_AUTO_RETALIATE = 172;
-    public static final int SETTING_TOGGLE_RUN = 173;
-
-    public static final int SETTING_BANK_TOGGLE_REARRANGE_MODE = 304;
-    public static final int SETTING_TOGGLE_ACCEPT_AID = 427;
-
-    public static final int SETTING_SPLIT_PRIVATE_CHAT = 287;
-
-    public static final int SETTING_ADJUST_AREA_SOUND_EFFECT_VOLUME = 872;
-
-    public static final int SETTING_SWAP_QUEST_DIARY = 1002;
-    public static final int SETTING_PRAYER_THICK_SKIN = 83;
-    public static final int SETTING_TOGGLE_LOOP_MUSIC = 19;
-    public static final int SETTING_BANK_TOGGLE_WITHDRAW_MODE = 115;
-    public static final int SETTING_TYPE_SHOP = 118;
+    /**
+	 * Permited values: 0 - 1
+     * Note: 0 = disabled, 1 = enabled
+	 */
+    public static final int SETTING_TOGGLE_RUN_ENABLED = 173;
+    public static final int SETTING_SPLIT_PRIVATE_CHAT = 287;    
+    /**
+	 * Permited values: 0 - 100
+     * Note: Holds the current special attack energy in percents.
+	 */
+    public static final int SETTING_SPECIAL_ATTACK_ENERGY = 300;  
+    /**
+	 * Permited values: 0 - 1
+     * Note: 0 = disabled, 1 = enabled
+	 */
     public static final int SETTING_SPECIAL_ATTACK_ENABLED = 301;
-
-
-
-    //Non-account settings (widget related ones)
+    public static final int SETTING_BANK_TOGGLE_REARRANGE_MODE = 304;    
+    public static final int SETTING_TOGGLE_ACCEPT_AID = 427;
+    public static final int SETTING_ACTIVE_MAGIC_BOOK = 439;
+    public static final int SETTING_ADJUST_AREA_SOUND_EFFECT_VOLUME = 872;
+    public static final int SETTING_SWAP_QUEST_DIARY = 1002;
+    // Blast furnance
+    public static final int SETTING_BLAST_FURNACE_COOLING_STATE = 543;
+    public static final int SETTING_BLAST_FURNACE_STORED_ORE = 547;    
+    public static final int SETTING_BLAST_FURNACE_COFFER_COINS = 795;
+    // Non-account settings (widget related ones)
     public static final int SPRITE_SELECTED_VALUE = 1079;
     public static final int SPRITE_UNSELECTED_VALUE = 1077;
-
 }
