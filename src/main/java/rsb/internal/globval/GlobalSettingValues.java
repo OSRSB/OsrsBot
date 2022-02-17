70150c840ebc1ad4c0c9cbf663263ee32f804517
Bia10 <t.locke1234@gmail.com>
2022-02-17 17:55:21 +0100

    fixes

--- src/main/java/rsb/internal/globval/GlobalSettingValues.java
+++ src/main/java/rsb/internal/globval/GlobalSettingValues.java
@@ -1,45 +1,51 @@
 package rsb.internal.globval;
 
 public class GlobalSettingValues {
-    public static final int SETTING_TOGGLE_LOOP_MUSIC = 19;  
-    public static final int SETTING_COMBAT_STYLE = 43;
-    public static final int SETTING_PRAYER_THICK_SKIN = 83;
-    public static final int SETTING_QUEST_POINTS = 101;
-    public static final int SETTING_POISON = 102;
-    public static final int SETTING_BANK_TOGGLE_WITHDRAW_MODE = 115;
-    public static final int SETTING_TYPE_SHOP = 118; 
-    public static final int SETTING_ADJUST_SCREEN_BRIGHTNESS = 166;
-    public static final int SETTING_ADJUST_MUSIC_VOLUME = 168;
-    public static final int SETTING_ADJUST_SOUND_EFFECT_VOLUME = 169;
-    public static final int SETTING_MOUSE_BUTTONS = 170;
-    public static final int SETTING_CHAT_EFFECTS = 171;
-    public static final int SETTING_AUTO_RETALIATE = 172;
+    // Player/account
+    public static final int PLAYER_VENOM_STATE = 12;
+    public static final int TOGGLE_LOOP_MUSIC = 19;
+    public static final int COMBAT_STYLE = 43;
+    public static final int PLAYER_POISONED_STATE = 83;
+    public static final int QUEST_POINTS = 101;
+    public static final int POISON = 102;
+    public static final int BANK_TOGGLE_WITHDRAW_MODE = 115;
+    public static final int TYPE_SHOP = 118;
+    public static final int ADJUST_SCREEN_BRIGHTNESS = 166;
+    public static final int ADJUST_MUSIC_VOLUME = 168;
+    public static final int ADJUST_SOUND_EFFECT_VOLUME = 169;
+    public static final int MOUSE_BUTTONS = 170;
+    public static final int CHAT_EFFECTS = 171;
     /**
-	 * Permited values: 0 - 1
+     * Permitted values: 0 - 1
      * Note: 0 = disabled, 1 = enabled
-	 */
-    public static final int SETTING_TOGGLE_RUN_ENABLED = 173;
-    public static final int SETTING_SPLIT_PRIVATE_CHAT = 287;    
+     */
+    public static final int AUTO_RETALIATE_ENABLED = 172;
     /**
-	 * Permited values: 0 - 100
+     * Permitted values: 0 - 1
+     * Note: 0 = disabled, 1 = enabled
+     */
+    public static final int TOGGLE_RUN_ENABLED = 173;
+    public static final int SPLIT_PRIVATE_CHAT = 287;
+    /**
+     * Permitted values: 0 - 100
      * Note: Holds the current special attack energy in percents.
-	 */
-    public static final int SETTING_SPECIAL_ATTACK_ENERGY = 300;  
+     */
+    public static final int SPECIAL_ATTACK_ENERGY = 300;
     /**
-	 * Permited values: 0 - 1
+     * Permitted values: 0 - 1
      * Note: 0 = disabled, 1 = enabled
-	 */
-    public static final int SETTING_SPECIAL_ATTACK_ENABLED = 301;
-    public static final int SETTING_BANK_TOGGLE_REARRANGE_MODE = 304;    
-    public static final int SETTING_TOGGLE_ACCEPT_AID = 427;
-    public static final int SETTING_ACTIVE_MAGIC_BOOK = 439;
-    public static final int SETTING_ADJUST_AREA_SOUND_EFFECT_VOLUME = 872;
-    public static final int SETTING_SWAP_QUEST_DIARY = 1002;
-    // Blast furnance
-    public static final int SETTING_BLAST_FURNACE_COOLING_STATE = 543;
-    public static final int SETTING_BLAST_FURNACE_STORED_ORE = 547;    
-    public static final int SETTING_BLAST_FURNACE_COFFER_COINS = 795;
+     */
+    public static final int SPECIAL_ATTACK_ENABLED = 301;
+    public static final int BANK_TOGGLE_REARRANGE_MODE = 304;
+    public static final int TOGGLE_ACCEPT_AID = 427;
+    public static final int ACTIVE_MAGIC_BOOK = 439;
+    public static final int ADJUST_AREA_SOUND_EFFECT_VOLUME = 872;
+    public static final int SWAP_QUEST_DIARY = 1002;
+    // Blast furnace
+    public static final int BLAST_FURNACE_COOLING_STATE = 543;
+    public static final int BLAST_FURNACE_STORED_ORE = 547;
+    public static final int BLAST_FURNACE_COFFER_COINS = 795;
     // Non-account settings (widget related ones)
-    public static final int SPRITE_SELECTED_VALUE = 1079;
     public static final int SPRITE_UNSELECTED_VALUE = 1077;
+    public static final int SPRITE_SELECTED_VALUE = 1079;
 }
