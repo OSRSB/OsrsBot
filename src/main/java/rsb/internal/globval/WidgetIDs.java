136aa772f944ab76250daa0dd544ce7245e59181
Bia10 <t.locke1234@gmail.com>
2022-03-02 22:17:05 +0100

    minimap done

---
+++ src/main/java/rsb/internal/globval/WidgetIDs.java
@@ -0,0 +1,58 @@
+package rsb.internal.globval;
+
+public class WidgetIDs {
+    /**
+     * Describes minimap widgets
+     * Last reviewed: 28/2/2022 1:57 am UTC+1
+     */
+    public static class Minimap {
+        public static final int GROUP_ID = 160;
+        public static final int XP_ORB_SPRITE = 1; // Interactions: "Hide", "Setup"
+        public static final int HEALTH_ORB_PARENT_CONTAINER = 2;
+        public static final int HEALTH_ORB_PARENT_SPRITE = 3;
+        public static final int HEALTH_ORB_CONTAINER = 4; // Interactions: "Cure"
+        public static final int HEALTH_ORB_LABEL = 5;
+        public static final int HEALTH_ORB_SPRITE = 6;
+        // 7 - 8 gap
+        public static final int HEALTH_ORB_ENERGY_CONSUMED_CONTAINER = 9;
+        public static final int HEALTH_ORB_ENERGY_CONSUMED_SPRITE = 10;
+        public static final int HEALTH_ORB_ICON_CONTAINER = 11;
+        public static final int HEALTH_ORB_ICON_SPRITE = 12;
+        public static final int QUICK_PRAYER_ORB_PARENT_CONTAINER = 13;
+        public static final int QUICK_PRAYER_ORB_PARENT_SPRITE = 14;
+        public static final int QUICK_PRAYER_ORB_CONTAINER = 15; // Interactions: "Activate", "Setup"
+        public static final int QUICK_PRAYER_ORB_LABEL = 16;
+        public static final int QUICK_PRAYER_ORB_SPRITE = 17;
+        public static final int QUICK_PRAYER_ORB_ENERGY_CONSUMED_CONTAINER = 18;
+        public static final int QUICK_PRAYER_ORB_ENERGY_CONSUMED_SPRITE = 19;
+        public static final int QUICK_PRAYER_ORB_ICON_SPRITE = 20;
+        public static final int RUN_ORB_PARENT_CONTAINER = 21;
+        public static final int RUN_ORB_PARENT_SPRITE = 22;
+        public static final int RUN_ORB_CONTAINER = 23; // Interactions: "Toggle"
+        public static final int RUN_ORB_LABEL = 24;
+        public static final int RUN_ORB_SPRITE = 25;
+        public static final int RUN_ORB_ENERGY_CONSUMED_CONTAINER = 26;
+        public static final int RUN_ORB_ENERGY_CONSUMED_SPRITE = 27;
+        public static final int RUN_ORB_ICON_SPRITE = 28;
+        public static final int SPEC_ORB_PARENT_CONTAINER = 29;
+        public static final int SPEC_ORB_PARENT_SPRITE = 30;
+        public static final int SPEC_ORB_CONTAINER = 31; // Interactions: "Use"
+        public static final int SPEC_ORB_LABEL = 32;
+        public static final int SPEC_ORB_ICON_SPRITE = 33;
+        public static final int SPEC_ORB_ENERGY_CONSUMED_CONTAINER = 34;
+        public static final int SPEC_ORB_ENERGY_CONSUMED_SPRITE = 35;
+        public static final int SPEC_ORB_RECHARGE_CONTAINER = 36; // NOTE: unclear
+        public static final int SPEC_ORB_SPRITE = 37;
+        public static final int BOND_ORB_PARENT_CONTAINER = 38;
+        public static final int BOND_ORB_PARENT_SPRITE = 39;
+        public static final int BOND_ORB_ICON_SPRITE = 40;
+        public static final int BOND_ORB_CONTAINER = 41; // Interactions: "Open Store", "Bond Pouch"
+        public static final int BOND_ORB_SPRITE = 42;
+        public static final int WORLDMAP_ORB_CONTAINER = 43;
+        public static final int WIKI_BANNER_PARENT_CONTAINER = 44;
+        public static final int WIKI_BANNER_CONTAINER = 45; // Interactions: ... 10 unknown
+        public static final int WIKI_BANNER_SPRITE = 46;
+        public static final int WORLDMAP_ORB_PARENT_SPRITE = 47;
+        public static final int WORLDMAP_ORB_SPRITE = 48; // Interactions: "Floating World Map", "Fullscreen World Map"
+    }
+}
