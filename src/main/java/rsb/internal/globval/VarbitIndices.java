73cb5c3c1cf744700670beae64da49d989cbf528
Bia10 <t.locke1234@gmail.com>
2022-03-22 14:32:09 +0100

    add VarbitIndices

---
+++ src/main/java/rsb/internal/globval/VarbitIndices.java
@@ -0,0 +1,15 @@
+package rsb.internal.globval;
+
+public class VarbitIndices {
+    public static final int SPELL_TO_AUTOCAST = 276; // Permitted values: WidgetIndices.DynamicComponents.AutocastSpells
+    public static final int EQUIPPED_WEAPON_TYPE = 357;
+    public static final int DEFENSIVE_AUTOCAST = 2668;
+    public static final int QUICK_PRAYER = 4103;
+    public static final int CURRENT_BANK_TAB = 4150;
+    public static final int XP_DROPS = 4702;
+    public static final int FAIRY_RING_DIAL_ADCB = 3985; // Left dial
+    public static final int FAIRY_RING_DIAL_ILJK = 3986; // Middle dial
+    public static final int FAIRY_RING_DIAL_PSRQ = 3987; // Right dial
+    public static final int FAIR_RING_LAST_DESTINATION = 5374;
+    public static final int QUEST_TAB_INDEX = 6590;
+}
