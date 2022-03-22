16bffc117b492d4a35bc763c093b86d8254160f7
Bia10 <t.locke1234@gmail.com>
2022-03-22 23:32:27 +0100

    magic filter done

--- src/main/java/rsb/internal/globval/VarbitIndices.java
+++ src/main/java/rsb/internal/globval/VarbitIndices.java
@@ -11,9 +11,15 @@ public class VarbitIndices {
     public static final int FAIRY_RING_MIDDLE_DIAL_ILJK = 3986;
     public static final int FAIRY_RING_RIGHT_DIAL_PSRQ = 3987;
     public static final int FAIRY_RING_LAST_DESTINATION = 5374;
+    public static final int MAGIC_FILTER_SHOW_COMBAT_SPELLS = 6605;
+    public static final int MAGIC_FILTER_SHOW_UTILITY_SPELLS = 6606;
+    public static final int MAGIC_FILTER_SHOW_NO_LEVEL_SPELLS = 6607;
+    public static final int MAGIC_FILTER_SHOW_NO_RUNES_SPELLS = 6608;
+    public static final int MAGIC_FILTER_SHOW_TELEPORT_SPELLS = 6609;
     public static final int TOGGLE_MOUSE_WHEEL_ZOOM = 6357;
     public static final int TAB_QUEST_SUBTAB_INDEX = 8168;
     public static final int TAB_SETTINGS_SUBTAB_INDEX = 9683;
     public static final int TAB_ACC_MANAGEMENT_SUBTAB_INDEX = 10060;
+    public static final int MAGIC_FILTER_SHOW_LACKING_REQUIREMENTS_SPELLS = 12137;
     public static final int TAB_CHAT_CHANNEL_SUBTAB_INDEX = 13071;
 }
