bd75a78bfdfae81fd9191d9d84954abe09382078
Bia10 <t.locke1234@gmail.com>
2022-03-22 19:03:48 +0100

    refactors/fixes

--- src/main/java/rsb/internal/globval/VarbitIndices.java
+++ src/main/java/rsb/internal/globval/VarbitIndices.java
@@ -7,9 +7,9 @@ public class VarbitIndices {
     public static final int QUICK_PRAYER = 4103;
     public static final int CURRENT_BANK_TAB = 4150;
     public static final int XP_DROPS = 4702;
-    public static final int FAIRY_RING_DIAL_ADCB = 3985; // Left dial
-    public static final int FAIRY_RING_DIAL_ILJK = 3986; // Middle dial
-    public static final int FAIRY_RING_DIAL_PSRQ = 3987; // Right dial
-    public static final int FAIR_RING_LAST_DESTINATION = 5374;
-    public static final int QUEST_TAB_INDEX = 6590;
+    public static final int FAIRY_RING_LEFT_DIAL_ADCB = 3985;
+    public static final int FAIRY_RING_MIDDLE_DIAL_ILJK = 3986;
+    public static final int FAIRY_RING_RIGHT_DIAL_PSRQ = 3987;
+    public static final int FAIRY_RING_LAST_DESTINATION = 5374;
+    public static final int QUEST_TAB_INDEX = 8168;
 }
