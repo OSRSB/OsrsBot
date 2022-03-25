6116ffcb1cf11a1213f00966515d26cfe68b86c3
Bia10 <t.locke1234@gmail.com>
2022-03-25 12:10:06 +0100

    refactor RSItem

--- src/main/java/rsb/wrappers/RSItem.java
+++ src/main/java/rsb/wrappers/RSItem.java
@@ -1,10 +1,7 @@
 package rsb.wrappers;
 
-
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.Item;
 import net.runelite.api.ItemComposition;
-import net.runelite.api.Tile;
 import net.runelite.api.TileItem;
 import rsb.methods.MethodContext;
 import rsb.methods.MethodProvider;
@@ -13,10 +10,7 @@ import rsb.wrappers.common.Clickable07;
 import rsb.wrappers.subwrap.RSMenuNode;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.Arrays;
-import java.util.concurrent.Callable;
 import java.util.function.Predicate;
 
 /**
@@ -25,9 +19,8 @@ import java.util.function.Predicate;
  */
 @Slf4j
 public class RSItem extends MethodProvider implements Clickable07 {
-
 	private final int id;
-	private final int stack;
+	private final int stackSize;
 	private RSWidget component;
 	private RSWidgetItem item;
 	private static Field groundActionMethod;
@@ -35,22 +28,21 @@ public class RSItem extends MethodProvider implements Clickable07 {
 	public RSItem(final MethodContext ctx, final RSWidgetItem item) {
 		super(ctx);
 		this.id = item.getItemId();
-		this.stack = item.getStackSize();
+		this.stackSize = item.getStackSize();
 		this.item = item;
 	}
 
 	public RSItem(final MethodContext ctx, final TileItem item) {
 		super(ctx);
 		id = item.getId();
-		stack = item.getQuantity();
+		stackSize = item.getQuantity();
 		//This is only used for ground objects and thus does not need component declared
 	}
-
 
 	public RSItem(final MethodContext ctx, final RSWidget item) {
 		super(ctx);
 		id = item.getItemId();
-		stack = item.getStackSize();
+		stackSize = item.getStackSize();
 		component = item;
 	}
 
@@ -78,7 +70,7 @@ public class RSItem extends MethodProvider implements Clickable07 {
 	 * @return The stack size.
 	 */
 	public int getStackSize() {
-		return stack;
+		return stackSize;
 	}
 
 	/**
@@ -91,7 +83,8 @@ public class RSItem extends MethodProvider implements Clickable07 {
 		return getDefinition() != null;
 	}
 
-	/**Gets the item wrapped by this RSItem
+	/**
+	 * Gets the item wrapped by this RSItem
 	 *
 	 * @return The wrapped item or <code>null</code>
 	 */
@@ -126,7 +119,6 @@ public class RSItem extends MethodProvider implements Clickable07 {
 		return item.isValid();
 	}
 
-
 	/**
 	 * Gets the name of this item using the wrapped component's name
 	 * if available, otherwise the definition if available.
@@ -134,7 +126,6 @@ public class RSItem extends MethodProvider implements Clickable07 {
 	 * @return The item's name or <code>null</code> if not found.
 	 */
 	public String getName() {
-
 		if (component != null && component.getName() != null) {
 			return component.getName();
 		} else {
@@ -151,7 +142,7 @@ public class RSItem extends MethodProvider implements Clickable07 {
 	 * @return The list of inventory actions for the item
 	 */
 	public String[] getInventoryActions() {
-		if (id < 0 && stack < 0) {
+		if (id < 0 && stackSize < 0) {
 			return null;
 		}
 		ItemComposition definition = getDefinition();
@@ -162,7 +153,7 @@ public class RSItem extends MethodProvider implements Clickable07 {
 	}
 
 	public String[] getGroundActions() {
-		if (id < 0 && stack < 0) {
+		if (id < 0 && stackSize < 0) {
 			return new String[]{""};
 		}
 		if (getDefinition() != null) {
@@ -197,7 +188,6 @@ public class RSItem extends MethodProvider implements Clickable07 {
 		}
 	}
 
-
 	/**
 	 * Performs the given action on the component wrapped by
 	 * this RSItem if possible.
@@ -220,14 +210,16 @@ public class RSItem extends MethodProvider implements Clickable07 {
 	 *         successfully; otherwise <code>false</code>.
 	 */
 	public boolean doAction(final String action, final String option) {
-		return (component != null) ?  component.doAction(action, option) : item.doAction(action, option);
+		return (component != null) ? component.doAction(action, option) : item.doAction(action, option);
 	}
 
 	public boolean doAction(Predicate<RSMenuNode> predicate) {
 		component.doClick(false);
 		for (RSMenuNode menuNode : Web.methods.chooseOption.getMenuNodes()) {
 			if (predicate.test(menuNode)) {
-				return (component != null) ? component.doAction(menuNode.getAction(), menuNode.getTarget()) : item.doAction(menuNode.getAction(), menuNode.getTarget());
+				return (component != null)
+						? component.doAction(menuNode.getAction(), menuNode.getTarget())
+						: item.doAction(menuNode.getAction(), menuNode.getTarget());
 			}
 		}
 		return false;
@@ -249,11 +241,9 @@ public class RSItem extends MethodProvider implements Clickable07 {
 		return (component != null) ? component.doClick(true) : item.doClick(true);
 	}
 
-
 	public boolean doHover() {
 		return (component != null) && component.doHover();
 	}
-
 
 	public boolean isClickable() {
 		return component.isValid() && component.isVisible() && component.isSelfVisible();
