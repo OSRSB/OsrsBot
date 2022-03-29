2be955b0e7197a7b2b04daf937dee7f00788b96a
Bia10 <t.locke1234@gmail.com>
2022-03-29 23:50:26 +0100

    update VarcIntValues

--- src/main/java/rsb/methods/Game.java
+++ src/main/java/rsb/methods/Game.java
@@ -150,6 +150,7 @@ public class Game extends MethodProvider {
 			case TAB_CHAT_CHANNEL -> InterfaceTab.CHAT;
 			case TAB_ACC_MANAGEMENT -> InterfaceTab.ACC_MAN;
 			case TAB_EMOTES -> InterfaceTab.EMOTES;
+			default -> throw new IllegalStateException("Unexpected value: " + VarcIntValues.valueOf(varcIntValue));
 		};
 	}
 
