653357f53df1b78aa1162bb51ca61091dcca6a7e
Bia10 <t.locke1234@gmail.com>
2022-03-29 22:57:53 +0100

    update VarbitIndices

--- src/main/java/rsb/internal/globval/VarbitIndices.java
+++ src/main/java/rsb/internal/globval/VarbitIndices.java
@@ -4,22 +4,31 @@ public class VarbitIndices {
     public static final int SPELL_TO_AUTOCAST = 276; // Permitted values: WidgetIndices.DynamicComponents.AutocastSpells
     public static final int EQUIPPED_WEAPON_TYPE = 357;
     public static final int DEFENSIVE_AUTOCAST = 2668;
-    public static final int QUICK_PRAYER = 4103;
-    public static final int CURRENT_BANK_TAB = 4150;
-    public static final int XP_DROPS = 4702;
+    public static final int TOGGLE_BANK_ALWAYS_SET_PLACEHOLDERS = 3755;
+    public static final int TOGGLE_BANK_WITHDRAW_MODE = 3958;
+    public static final int TOGGLE_BANK_REARRANGE_MODE = 3959;
+    /**
+     * Permitted values: 0 - (2^31)-1
+     * Note: last integer value set when withdrawing with withdraw X option
+     */
+    public static final int BANK_DEFAULT_WITHDRAW_X_QUANTITY = 3960;
     public static final int FAIRY_RING_LEFT_DIAL_ADCB = 3985;
     public static final int FAIRY_RING_MIDDLE_DIAL_ILJK = 3986;
     public static final int FAIRY_RING_RIGHT_DIAL_PSRQ = 3987;
+    public static final int QUICK_PRAYER = 4103;
+    public static final int CURRENT_BANK_TAB = 4150;
+    public static final int TOGGLE_XP_DROPS = 4702;
     public static final int FAIRY_RING_LAST_DESTINATION = 5374;
-    public static final int MAGIC_FILTER_SHOW_COMBAT_SPELLS = 6605;
-    public static final int MAGIC_FILTER_SHOW_UTILITY_SPELLS = 6606;
-    public static final int MAGIC_FILTER_SHOW_NO_LEVEL_SPELLS = 6607;
-    public static final int MAGIC_FILTER_SHOW_NO_RUNES_SPELLS = 6608;
-    public static final int MAGIC_FILTER_SHOW_TELEPORT_SPELLS = 6609;
     public static final int TOGGLE_MOUSE_WHEEL_ZOOM = 6357;
-    public static final int TAB_QUEST_SUBTAB_INDEX = 8168;
+    public static final int BANK_WITHDRAW_QUANTITY = 6590;
+    public static final int TOGGLE_MAGIC_FILTER_COMBAT_SPELLS = 6605;
+    public static final int TOGGLE_MAGIC_FILTER_UTILITY_SPELLS = 6606;
+    public static final int TOGGLE_MAGIC_FILTER_NO_LEVEL_SPELLS = 6607;
+    public static final int TOGGLE_MAGIC_FILTER_NO_RUNES_SPELLS = 6608;
+    public static final int TOGGLE_MAGIC_FILTER_TELEPORT_SPELLS = 6609;
+    public static final int TAB_QUEST_LIST_SUBTAB_INDEX = 8168;
     public static final int TAB_SETTINGS_SUBTAB_INDEX = 9683;
     public static final int TAB_ACC_MANAGEMENT_SUBTAB_INDEX = 10060;
-    public static final int MAGIC_FILTER_SHOW_LACKING_REQUIREMENTS_SPELLS = 12137;
+    public static final int TOGGLE_MAGIC_FILTER_LACKING_REQUIREMENTS_SPELLS = 12137;
     public static final int TAB_CHAT_CHANNEL_SUBTAB_INDEX = 13071;
 }
