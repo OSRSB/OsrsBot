2646605cf33ef231b55c96f69daa923098b84933
Bia10 <t.locke1234@gmail.com>
2022-04-11 22:23:35 +0200

    add quest prince ali rescue

--- src/main/java/rsb/internal/globval/VarpIndices.java
+++ src/main/java/rsb/internal/globval/VarpIndices.java
@@ -136,6 +136,11 @@ public class VarpIndices {
      * Note: 0 = not started, 1 = started, 21 = finished
      */
     public static final int QUEST_SHEEP_SHEARER = 179;
+    /**
+     * Permitted values: 0,10,20,30,31,40,50,100,110
+     * Note: 0 = not started, 1 = started, 110 = finished
+     */
+    public static final int QUEST_PRINCE_ALI_RESCUE = 273;
     public static final int SPLIT_PRIVATE_CHAT = 287;
     /**
      * Permitted values: 0 - 100
